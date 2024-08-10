547ef55b0509f4b92c4a3872af4b975a83fff296
JPratama7 <josepratama080@gmail.com>
2024-08-11 00:07:50 +0700

    feat: add user registration and login

--- route/route.go
+++ route/route.go
@@ -1,8 +1,15 @@
 package route
 
 import (
+	"context"
+	"crud/controller"
+	"crud/repository"
+	"crud/util"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"os"
 )
 
 type HTTPMethod string
@@ -35,4 +42,28 @@ func (m *Muxer) AddRoute(method HTTPMethod, path string, handler func(http.Respo
 
 func (m *Muxer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.muxer.ServeHTTP(w, r)
+}
+
+func (m *Muxer) AddMiddleware(middleware mux.MiddlewareFunc) {
+	m.muxer.Use(middleware)
+}
+
+var Router *Muxer
+
+func init() {
+	Router = NewMuxer()
+
+	conn, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		panic(err)
+	}
+
+	db := conn.Database(os.Getenv("MONGODB_DB"))
+
+	userColl := repository.NewUserRepository(db, "users")
+
+	Router.AddMiddleware(util.InsertToRequest("userDb", userColl))
+
+	Router.AddRoute(POST, "/register", controller.Register)
+	Router.AddRoute(POST, "/login", controller.Login)
 }
